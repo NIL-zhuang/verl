049ccdb12b7ce189115084586392a2ad4a653a9e
zhuangziyuan <zhuangziyuan@meituan.com>
2025-03-25 22:52:25 +0800

    make llava-onevision evaluation more easy

--- verl/utils/reward_score/llava_onevision.py
+++ verl/utils/reward_score/llava_onevision.py
@@ -1,27 +1,14 @@
-from .mathruler.grader import extract_boxed_content
-from math_verify import ExprExtractionConfig, LatexExtractionConfig, StringExtractionConfig, parse, verify
+from .mathruler.grader import extract_boxed_content, grade_answer
 
 
 def compute_score(predict_str: str, ground_truth: str) -> float:
-    answer = extract_boxed_content(predict_str)
-    if ground_truth.lower() == str(answer).lower():
-        return 1.0
-
-    # parse gold answer
     try:
-        gold_parsed = parse(ground_truth)
-        if len(gold_parsed) == 0:
-            return 0.0
-    except Exception:
+        answer = extract_boxed_content(predict_str)
+        if ground_truth.lower() == str(answer).lower():
+            return 1.0
+        if grade_answer(answer, ground_truth):
+            return 1.0
+    except Exception as e:
+        print(e)
         return 0.0
-
-    # parse model prediction
-    try:
-        answer_parsed = parse(
-            predict_str, extraction_config=[StringExtractionConfig(), LatexExtractionConfig(), ExprExtractionConfig()]
-        )
-        reward = float(verify(answer_parsed, gold_parsed))
-    except Exception:
-        reward = 0.0
-
-    return reward
+    return 0.0
